755141367863ac6cbdeabf08e05841d82c6a8c74
teirm <tribunus.vii@gmail.com>
2016-12-05 21:04:56 -0500

    Formatting changes and added function defns to submit jobs

---
+++ job_submission.py
@@ -0,0 +1,57 @@
+"""
+Project: tiny-google.py
+Module: Job Submission
+Authors: Cyrus Ramavarapu and Therese Dachille
+Purpose: Functions for spawing MapReduce and SparkJobs
+"""
+
+import subprocess
+import shlex
+
+
+def submit_mr_index():
+    """
+    Will submit the indexing map reduce
+    job.
+
+    Keyword arguments:
+    doc_path -- path to the document to index
+    out_dir -- directory for output
+
+    Returns: Int
+    """
+
+
+def submit_mr_rar():
+    """
+    Will submit the map reduce rank and
+    retrieval job.
+
+    Keyword arguments:
+    UNSURE RIGHT NOW
+
+    Returns: Int
+    """
+
+
+def submit_spark_index():
+    """
+    Will submit the indexing spark job
+
+    Keyword arguments:
+    doc_path -- path to the document to index
+    out_dir -- directory for output
+
+    Returns: Int
+    """
+
+
+def submit_spark_rar():
+    """
+    Will submit the spark rank and retrieval job.
+
+    Keyword arguments:
+    UNSURE RIGHT NOW
+
+    Returns: Int
+    """
--- tiny-google.py
+++ tiny-google.py
@@ -1,33 +1,53 @@
-# tiny-google.py
-# Cyrus Ramavarapu and Therese Dachille
-# This program will allow the user to offer text documents to be indexed with a Map Reduce job.
-# This program will also allow the user to input keyword(s) to search for in the documents,
-# first having Map Reduce rank the documents, then having Python find the exact positions
-# of the keyword(s) in the retrieved document(s).
-
+"""
+Project: tiny-google.py
+Authors: Cyrus Ramavarapu and Therese Dachille
+Purpose: This program will allow the user to offer text
+         documents to be indexed with a Map Reduce job.
+         This program will also allow the user to input
+         keyword(s) to search for in the documents,
+         first having Map Reduce rank the documents,
+         then having Python find the exact positions
+         of the keyword(s) in the retrieved document(s).
+"""
 from sys import version_info
 
-py3 = version_info[0] > 2
+PY_3 = version_info[0] > 2
 
-def main ():
+def main():
     print('Welcome to tiny-google.\n')
 
-    if py3: 
-        choice = int(input('Would you like to:\n1) Input a document to index?\n2) Input keyword(s) to search for?\n'))
-    else: 
-        choice = int(raw_input('Would you like to:\n1) Input a document to index?\n2) Input keyword(s) to search for?\n'))
-    if choice == 1:
-        if py3:
-            doc_name = input("You have selected to index a document. What is the name of the document? ")
-        else:
-            doc_name = raw_input("You have selected to index a document. What is the name of the document? ")
-        
-    elif choice == 2:
-        if py3:
-            keyword = input("You have selected search for keyword(s). Please enter the keyword(s): ")
-        else:
-            keyword = raw_input("You have selected search for keyword(s). Please enter the keyword(s): ")
-        
+    if PY_3:
+        choice = int(input(
+            'Would you like to:\n\
+            1) Input a document to index?\n\
+            2) Input keyword(s) to search for?\n'))
+    else:
+        choice = int(raw_input(
+            'Would you like to:\n\
+            1) Input a document to index?\n\
+            2) Input keyword(s) to search for?\n'))
 
-if __name__=='__main__':
+    if choice == 1:
+        if PY_3:
+            doc_name = input(
+                "You have selected to index a document.\n\
+What is the name of the document? ")
+        else:
+            doc_name = raw_input(
+                "You have selected to index a document.\n\
+What is the name of the document? ")
+
+    elif choice == 2:
+        if PY_3:
+            keyword = input(
+                "You have selected search for keyword(s).\n\
+Please enter the keyword(s): ")
+        else:
+            keyword = raw_input(
+                "You have selected search for keyword(s).\n\
+Please enter the keyword(s): ")
+
+
+if __name__ == '__main__':
+    
     main()
