48207181bac19daf9c0d57efdc12fcbd3a335e28
teirm <tribunus.vii@gmail.com>
2016-12-11 15:38:21 -0500

    Swapped Keys

--- spark_work/scala_funtimes/adv_wc.scala
+++ spark_work/scala_funtimes/adv_wc.scala
@@ -1,6 +1,28 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 
+def mix_values(concat_val: String, frequency: Int):
+(String, String) = {
+   
+  //val split_key = input_key.split(":")
+
+  if (concat_val.split(":").length == 2) {
+  
+    val book_name = concat_val.split(":")(0)
+    val word = concat_val.split(":")(1) 
+    
+    val book_freq = book_name ++ ":" ++ frequency.toString
+
+    return (word, book_freq)
+  } else {
+    val book_name = concat_val.split(":")(0)
+    val book_freq = book_name ++ ":" ++ frequency.toString
+    
+    return ("SPACE", book_freq) 
+   } 
+      
+}
+
 
 val conf = new SparkConf()
 val sc = new SparkContext(conf)
@@ -8,11 +30,11 @@ val sc = new SparkContext(conf)
 val files = sc.wholeTextFiles("../../map_reduce_work/input_dir")
 
 //val splits = files.mapValues(line => line.split(" ")) 
-
-//files.keys.map(s => s.split("/").last).collect.foreach(println)
-//files.values.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey(_ + _).collect.foreach(println)
-
-//val got_books = files.mapKeys(s => s.split("/").last)
+//
+////files.keys.map(s => s.split("/").last).collect.foreach(println)
+////files.values.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey(_ + _).collect.foreach(println)
+//
+////val got_books = files.mapKeys(s => s.split("/").last)
 
 val cleaned_files = files.mapValues(s => s.replaceAll("""([\p{Punct}&&[^.]]|\b\p{IsLetter}{1,2}\b)\s*""", " ")).mapValues(word => word.replaceAll("\\p{C}", " ")).mapValues(word => word.replaceAll("\\p{P}", " "))
 val split_files = cleaned_files.flatMapValues(line => line.split(" ")).mapValues(word => word.trim)
@@ -21,6 +43,12 @@ val concat_files = stripped_files.map(tuple => tuple._1.split("/").last.trim ++ 
 
 val counts = concat_files.map(key => (key, 1)).reduceByKey(_ + _)
 
-counts.coalesce(1, true).saveAsTextFile("test_dir")
+//val flattened_tuple = counts.map(tuple => tuple._1.split(":").mkString(":") ++ ":" ++ tuple._2.toString)
+
+val flattend_tuple = counts.map(tuple => mix_values(tuple._1, tuple._2))
+
+//val altered_keys = flattened_tuple.map(tuple => mix_values(tuple._1))
+
+flattened_tuple.coalesce(1, true).saveAsTextFile("test_dir")
 
 System.exit(0)
