078eb1fd70b0b021a18e2d355cbfc9a95798c464
teirm <tribunus.vii@gmail.com>
2016-12-11 19:33:32 -0500

    Words are now lower cased

--- spark_work/scala_funtimes/adv_wc.scala
+++ spark_work/scala_funtimes/adv_wc.scala
@@ -30,7 +30,7 @@ val sc = new SparkContext(conf)
 val files = sc.wholeTextFiles("../../map_reduce_work/input_dir")
 
 val cleaned_files = files.mapValues(s => s.replaceAll("""([\p{Punct}&&[^.]]|\b\p{IsLetter}{1,2}\b)\s*""", " ")).mapValues(word => word.replaceAll("\\p{C}", " ")).mapValues(word => word.replaceAll("\\p{P}", " "))
-val split_files = cleaned_files.flatMapValues(line => line.split(" ")).mapValues(word => word.trim)
+val split_files = cleaned_files.flatMapValues(line => line.split(" ")).mapValues(word => word.trim.toLowerCase)
 val stripped_files = split_files.mapValues(word => word.replaceAll("""([\p{Punct}&&[^.]]|\b\p{IsLetter}{1,2}\b)\s*""", "")).mapValues(word => word.replaceAll("\\p{C}", ""))
 val concat_files = split_files.map(tuple => tuple._1.split("/").last.trim ++ ":" ++ tuple._2.trim)
 val counts = concat_files.map(key => (key, 1)).reduceByKey(_ + _)
