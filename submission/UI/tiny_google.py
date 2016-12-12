9d4f4a0d7bde035cd9d0e3e74015ad9dd1f9abeb
Therese Dachille <tcdachille@gmail.com>
2016-12-12 14:30:25 -0500

    changed formatting of output

---
+++ submission/UI/tiny_google.py
@@ -0,0 +1,126 @@
+"""
+Project: tiny-google.py
+Authors: Cyrus Ramavarapu and Therese Dachille
+Purpose: This program will allow the user to offer text
+         documents to be indexed with a Map Reduce job.
+         This program will also allow the user to input
+         keyword(s) to search for in the documents,
+         first having Map Reduce rank the documents,
+         then having Python find the exact positions
+         of the keyword(s) in the retrieved document(s).
+"""
+
+from sys import stdin
+import subprocess
+import shlex
+import job_submission
+
+
+def main():
+    """ main menu for indexing and searching """
+    print('Welcome to tiny-google.\n')
+
+    while True:
+        print('Would you like to:\n\
+            1) Index documents?\n\
+            2) Input keyword(s) to search for?\n\
+            3) Exit\n')
+        choice = int(stdin.readline().rstrip('\n'))
+
+        if choice == 1:
+            print("You have selected to index documents.\n")
+
+            print('Would you like to perform indexing using:\n\
+                    1) MapReduce\n\
+                    2) Spark\n')
+
+            app_choice = int(stdin.readline().rstrip('\n'))
+
+            if app_choice == 1:
+                print(
+                    'You have chosen to index with MapReduce.\nBeginning MapReduce job...\n')
+                job_submission.submit_mr_index()
+
+            elif app_choice == 2:
+                print('You have chosen to index with Spark.\nBeginning Spark job...\n')
+                job_submission.submit_spark_index()
+
+        elif choice == 2:
+            print('You have selected search for keyword(s).\n' \
+                    'Please enter the keyword(s) you would like to search for,'\
+                        'each separated by a space: ')
+            keywords = stdin.readline().rstrip('\n')
+
+            print('\nHow many results would you like to return? ')
+            num_results = int(stdin.readline().rstrip('\n'))
+
+            print('And would you like to perform search using:\n\
+                    1) MapReduce\n\
+                    2) Spark\n')
+
+            app_choice2 = int(stdin.readline().rstrip('\n'))
+            app = ""
+            if app_choice2 == 1:
+                app = "MapReduce"
+            elif app_choice2 == 2:
+                app = "Spark"
+
+            keys_list = keywords.split(" ")
+
+            keys_str = ''
+            
+            # format to be 'word1', 'word2', and 'word3'
+            for x in range(0,len(keys_list)):
+                if x == len(keys_list)-1:
+                    keys_str += "and '" + keys_list[x] + "'"
+                else:
+                    keys_str += "'" + keys_list[x] + "', "
+
+
+            str_to_prnt = 'You have chosen to search for the top {0} documents'\
+                ' for {1} with {2}.\nBeginning {2} job...\n'.format(
+                    num_results, keys_str, app)
+
+            print(str_to_prnt)
+
+            if app_choice2 == 1:
+                job_submission.submit_mr_rar(keywords, num_results)
+                # make view:
+                subprocess.run(shlex.split('cat ./top_n_results/part-00000'))
+
+            elif app_choice2 == 2:
+                #job_submission.submit_spark_rar(keywords, num_results)
+                # make view:
+                #subprocess.run(shlex.split('cat ./search_results/part-00000'))
+                # read file
+                with open('./search_results/part-00000', 'r') as f:
+                    read_data = f.readlines()
+                f.closed
+
+                for line in read_data:
+                    word = line.split(':')[0]
+                    results = line.split(':')[1]
+                    print(word, '\n')
+                    each_result = results.split(')')
+                    for res in each_result:
+                        res = res[1:]
+                        print(res, '\n')
+
+
+
+
+
+        elif choice == 3:
+             # make clean:
+            # subprocess.run(shlex.split('rm -r ./output/'))
+            # subprocess.run(shlex.split('rm -r ./output_2/'))
+            # subprocess.run(shlex.split('rm -r ./search_results/'))
+            # subprocess.run(shlex.split('rm -r ./top_n_results/'))
+            # subprocess.run(shlex.split('rm -r ./inverted_index/'))
+            # subprocess.run(shlex.split('rm -r ./search_results/'))
+            print('Good-bye!')
+            exit(0)
+
+if __name__ == '__main__':
+
+    main()
