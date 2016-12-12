66b9a2ef41c861d995b49f84ea24785a4b88501f
teirm <tribunus.vii@gmail.com>
2016-12-11 19:32:18 -0500

    Cleaned up inverted index generation a bit

--- spark_work/scala_funtimes/adv_wc.scala
+++ spark_work/scala_funtimes/adv_wc.scala
@@ -3,24 +3,24 @@ import org.apache.spark.SparkConf
 
 def mix_values(concat_val: String, frequency: Int):
 (String, String) = {
-   
-  //val split_key = input_key.split(":")
+
+  var word : String = ""
+  var book_freq : String = ""
 
   if (concat_val.split(":").length == 2) {
   
     val book_name = concat_val.split(":")(0)
-    val word = concat_val.split(":")(1) 
-    
-    val book_freq = book_name ++ ":" ++ frequency.toString
+    word = concat_val.split(":")(1) 
+    book_freq = book_name ++ ":" ++ frequency.toString
 
-    return (word, book_freq)
+  
   } else {
     val book_name = concat_val.split(":")(0)
-    val book_freq = book_name ++ ":" ++ frequency.toString
-    
-    return ("SPACE", book_freq) 
+    word = "~SPACE~" 
+    book_freq = book_name ++ ":" ++ frequency.toString
    } 
-      
+  
+  return (word, book_freq)
 }
 
 
@@ -29,26 +29,14 @@ val sc = new SparkContext(conf)
 
 val files = sc.wholeTextFiles("../../map_reduce_work/input_dir")
 
-//val splits = files.mapValues(line => line.split(" ")) 
-//
-////files.keys.map(s => s.split("/").last).collect.foreach(println)
-////files.values.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey(_ + _).collect.foreach(println)
-//
-////val got_books = files.mapKeys(s => s.split("/").last)
-
 val cleaned_files = files.mapValues(s => s.replaceAll("""([\p{Punct}&&[^.]]|\b\p{IsLetter}{1,2}\b)\s*""", " ")).mapValues(word => word.replaceAll("\\p{C}", " ")).mapValues(word => word.replaceAll("\\p{P}", " "))
 val split_files = cleaned_files.flatMapValues(line => line.split(" ")).mapValues(word => word.trim)
 val stripped_files = split_files.mapValues(word => word.replaceAll("""([\p{Punct}&&[^.]]|\b\p{IsLetter}{1,2}\b)\s*""", "")).mapValues(word => word.replaceAll("\\p{C}", ""))
-val concat_files = stripped_files.map(tuple => tuple._1.split("/").last.trim ++ ":" ++ tuple._2.trim)
-
+val concat_files = split_files.map(tuple => tuple._1.split("/").last.trim ++ ":" ++ tuple._2.trim)
 val counts = concat_files.map(key => (key, 1)).reduceByKey(_ + _)
-
-//val flattened_tuple = counts.map(tuple => tuple._1.split(":").mkString(":") ++ ":" ++ tuple._2.toString)
-
 val swapped_keys = counts.map(tuple => mix_values(tuple._1, tuple._2))
-
 val inverted_index = swapped_keys.reduceByKey(_ ++ "->" ++ _)
 
-inverted_index.coalesce(1, true).saveAsTextFile("test_dir")
+inverted_index.coalesce(1, true).saveAsTextFile("inverted_index")
 
 System.exit(0)
